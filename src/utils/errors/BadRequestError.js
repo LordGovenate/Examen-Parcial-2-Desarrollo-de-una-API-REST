01cba0704829ebefb8aa6690b0210aa8a2b1f028
Johanhash <jjuarez53@alumnos.uaq.mx>
2025-04-20 21:00:30 -0600

    Add Autthentication

--- src/utils/errors/BadRequestError.js
+++ src/utils/errors/BadRequestError.js
@@ -1,9 +1,9 @@
 const HttpError = require('./HttpError');
-
-class BadRequestError extends HttpError {
-  constructor(message = 'Invalid request') {
-    super(message, 400);
-  }
-}
+  
+class BadRequestError extends HttpError { 
+  constructor(message = 'Invalid request') {    
+    super(message, 400);  
+  } 
+}   
 
 module.exports = BadRequestError;
